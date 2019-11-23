116bafc25ab36292a3786d2f22dd3e8f377e4205
Dave Augustus <davetweetlive@gmail.com>
2019-11-24 00:39:47 +0530

    Added fresh config to run changes without server restart

--- main.go
+++ main.go
@@ -3,19 +3,20 @@ package main
 import (
     "fmt"
 	"net/http"
-	// "log"
+	"log"
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "<h1> Welcome to the twitter Statics </h1>")
+	fmt.Fprint(w, "<h1> Welcome to the Twitter Statics </h1>")
 }
 
 func main() {
 	
 	//Go server resides here 
 	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":8080", nil)
-	// if err := http.ListenAndServe(":8080", nil); err != nil {
-	// 	log.Fatal("ListenAndServe: ", err)
-	// }
+	if err := http.ListenAndServe(":8080", nil); 
+	
+	err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
