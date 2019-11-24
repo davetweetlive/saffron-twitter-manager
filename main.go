a66375bd1fe1e97554443df470e73c617a45586b
Dave Augustus <davetweetlive@gmail.com>
2019-11-24 23:15:01 +0530

    annoynomous commit

--- main.go
+++ main.go
@@ -1,22 +1,39 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
-	"log"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "<h1> Welcome to the Twitter Statics </h1>")
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", helloHandler).Methods("GET")
+	r.HandleFunc("/bye", byeHandler).Methods("GET")
+	http.Handle("/", r)
+	http.ListenAndServe(":8080", nil)
 }
 
-func main() {
-	
-	//Go server resides here 
-	http.HandleFunc("/", handlerFunc)
-	if err := http.ListenAndServe(":8080", nil); 
-	
-	err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+
+func helloHandler(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprint(w, "Hello World!")
 }
+func byeHandler(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprint(w, "Good Bye!")
+}
+
+
+
+
+
+
+
+// func handlerFunc(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "text/html")
+// 	fmt.Fprint(w, "<h1> The Application is under construction! </h1>")
+// 	// if r.URL.Path == "/" {
+// 	//
+// 	// } else {
+// 	// 	w.WriteHeader(http.StatusNotFound)
+// 	// }
+// }
