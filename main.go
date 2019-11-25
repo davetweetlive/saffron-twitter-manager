66b25c42f3f1e3e7cc29ec3b8ce765b8bf12cc27
Dave Augustus <davetweetlive@gmail.com>
2019-11-25 23:31:32 +0530

    Integrated with mysql database

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ var templates *template.Template
 
 func main() {
 	connectToTheDatabase()
-	
+
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 
 	r := mux.NewRouter()
@@ -22,18 +22,28 @@ func main() {
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
 
-
 }
 
 
 // Mysql database connection db open db close, earror handle and success notification on terminal
 func connectToTheDatabase()  {
-	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/dbname")
+	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/database")
+
 	if err != nil{
 		panic(err.Error())
 	}
 	defer db.Close()
+
+	insert, err := db.Query("INSERT INTO Persons (PersonID, FirstName, LastName) VALUES(2, 'Pappu', 'Kumar')")
+
+	if err != nil{
+		panic(err.Error())
+		fmt.Println("There is a problem in Insert Query!")
+	}
+	defer insert.Close()
+
 	fmt.Println("Successful")
+
 }
 
 
