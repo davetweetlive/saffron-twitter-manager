80ef010fada67643f2db3ac10138ce6e752a24bf
Dave Augustus <davetweetlive@gmail.com>
2020-01-11 01:44:46 +0530

    Learning day 1

--- main.go
+++ main.go
@@ -1,70 +1,58 @@
 package main
 
 import (
-	"fmt"
-	"database/sql"
-	_"github.com/go-sql-driver/mysql"
-	"github.com/gorilla/mux"
-	"net/http"
-	"html/template"
+	"log"
+	"os"
+	"text/template"
+
 )
 
 var templates *template.Template
 
 func main() {
-	connectToTheDatabase()
-	templates = template.Must(template.ParseGlob("templates/*.html"))
-
-	r := mux.NewRouter()
-	r.HandleFunc("/", indexHandler).Methods("GET")
-
-	http.Handle("/", r)
-	http.ListenAndServe(":8080", nil)
-
-}
-
-
-// Mysql database connection db open db close, earror handle and success notification on terminal
-func connectToTheDatabase()  {
-	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/database")
+	tpl, err := template.ParseFiles("tpl.gohtml") // does nothing but gets reads vales of tpl.gohtml
 
 	if err != nil{
-		panic(err.Error())
+		log.Fatalln(err)
 	}
-	defer db.Close()
 
-	insert, err := db.Query("INSERT INTO Persons (PersonID, FirstName, LastName) VALUES(2, 'Pappu', 'Kumar')")
-
+	nf, err := os.Create("index.html") // Creates an html file
 	if err != nil{
-		panic(err.Error())
-		fmt.Println("There is a problem in Insert Query!")
+		log.Println("Error containing file", err)
 	}
-	defer insert.Close()
+	defer nf.Close()
 
-	fmt.Println("Successful")
+	err = tpl.Execute(nf, nil) // puts value og gpl.gohtml into index.html
+	if err != nil{
+		log.Fatalln(err)
+	}
 
+// adding some more files in tpl to parse
+	tpl, err = template.ParseFiles("one.gmao")
+	if err != nil{
+		log.Fatalln(err)
+	}
+
+	err = tpl.Execute(os.Stdout, nil)
+	if err!= nil{
+		log.Fatalln(err)
+	}
+
+	tpl,err = tpl.ParseFiles("two.gmao", "vespa.gmao")
+	if err!= nil{
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "vespa.gmao", nil)
+	if err!= nil{
+		log.Fatalln(err)
+	}
+	err = tpl.ExecuteTemplate(os.Stdout, "two.gmao", nil)
+	if err!= nil{
+		log.Fatalln(err)
+	}
+	err = tpl.ExecuteTemplate(os.Stdout, "one.gmao", nil)
+	if err!= nil{
+		log.Fatalln(err)
+	}
 }
-
-
-// Index Page which handles all the method and route performed on the index page
-func indexHandler(w http.ResponseWriter, r *http.Request)  {
-	fmt.Println("Welcome to index page")
-	templates.ExecuteTemplate(w, "index.html", nil)
-}
-
-
-
-
-
-
-
-
-// func handlerFunc(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "text/html")
-// 	fmt.Fprint(w, "<h1> The Application is under construction! </h1>")
-// 	// if r.URL.Path == "/" {
-// 	//
-// 	// } else {
-// 	// 	w.WriteHeader(http.StatusNotFound)
-// 	// }
-// }
