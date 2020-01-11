d0c4805a19f5c7b4bf9147e2ccb083a2684ffe4a
Dave Augustus <davetweetlive@gmail.com>
2020-01-12 00:52:52 +0530

    Added the index page html and a css file

--- main.go
+++ main.go
@@ -1,70 +1,33 @@
 package main
 
 import (
+	"io"
+	"net"
+	"log"
 	"fmt"
-	"database/sql"
-	_"github.com/go-sql-driver/mysql"
-	"github.com/gorilla/mux"
-	"net/http"
-	"html/template"
 )
 
-var templates *template.Template
+// var templates *template.Template
 
 func main() {
-	connectToTheDatabase()
-	templates = template.Must(template.ParseGlob("templates/*.html"))
+	// Establishing a server and connection over http
+	li, err := net.Listen("tcp", ":8080")
 
-	r := mux.NewRouter()
-	r.HandleFunc("/", indexHandler).Methods("GET")
-
-	http.Handle("/", r)
-	http.ListenAndServe(":8080", nil)
-
-}
-
-
-// Mysql database connection db open db close, earror handle and success notification on terminal
-func connectToTheDatabase()  {
-	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/database")
-
-	if err != nil{
-		panic(err.Error())
+	if err!= nil{
+		log.Panic(err)
 	}
-	defer db.Close()
 
-	insert, err := db.Query("INSERT INTO Persons (PersonID, FirstName, LastName) VALUES(2, 'Pappu', 'Kumar')")
+	defer li.Close()
+	fmt.Println(li)
 
-	if err != nil{
-		panic(err.Error())
-		fmt.Println("There is a problem in Insert Query!")
+	for{
+		conn, err := li.Accept()
+		if err!= nil{
+			log.Println(err)
+		}
+
+		io.WriteString(conn, "Hello there!, TCP connection has been establiched!")
+
+		conn.Close()
 	}
-	defer insert.Close()
-
-	fmt.Println("Successful")
-
 }
-
-
-// Index Page which handles all the method and route performed on the index page
-func indexHandler(w http.ResponseWriter, r *http.Request)  {
-	fmt.Println("Welcome to index page")
-	templates.ExecuteTemplate(w, "index.html", nil)
-}
-
-
-
-
-
-
-
-
-// func handlerFunc(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "text/html")
-// 	fmt.Fprint(w, "<h1> The Application is under construction! </h1>")
-// 	// if r.URL.Path == "/" {
-// 	//
-// 	// } else {
-// 	// 	w.WriteHeader(http.StatusNotFound)
-// 	// }
-// }
