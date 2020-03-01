b34a6f939f7661e624ddfdc2d8555cfb9d3cd542
Dave Augustus <davetweetlive@gmail.com>
2020-03-01 23:12:15 +0530

    Template ready

--- main.go
+++ main.go
@@ -1,72 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"twitter-stat/controller"
+	"twitter-stat/model"
 	"log"
-	"os"
-	"os/signal"
-	"syscall"
-
-	"github.com/dghubble/go-twitter/twitter"
-	"github.com/twitter-stat/config"
+	"net/http"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	client := config.TwitterClient()
-	fmt.Println(client)
+	r := mux.NewRouter()
+	r.HandleFunc("/register", controller.RegisterHandler).
+		Methods("POST")
+	r.HandleFunc("/login", controller.LoginHandler).
+		Methods("POST")
+	r.HandleFunc("/profile", controller.ProfileHandler).
+		Methods("GET")
 
-	// Convenience Demux demultiplexed stream messages
-	demux := twitter.NewSwitchDemux()
-	demux.Tweet = func(tweet *twitter.Tweet) {
-		fmt.Println(tweet.Text)
-	}
-	demux.DM = func(dm *twitter.DirectMessage) {
-		fmt.Println(dm.SenderID)
-	}
-	demux.Event = func(event *twitter.Event) {
-		fmt.Printf("%#v\n", event)
-	}
-
-	fmt.Println("Starting Stream...")
-
-	// FILTER
-	filterParams := &twitter.StreamFilterParams{
-		Track:         []string{"cat"},
-		StallWarnings: twitter.Bool(true),
-	}
-	stream, err := client.Streams.Filter(filterParams)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// USER (quick test: auth'd user likes a tweet -> event)
-	// userParams := &twitter.StreamUserParams{
-	// 	StallWarnings: twitter.Bool(true),
-	// 	With:          "followings",
-	// 	Language:      []string{"en"},
-	// }
-	// stream, err := client.Streams.User(userParams)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// SAMPLE
-	// sampleParams := &twitter.StreamSampleParams{
-	// 	StallWarnings: twitter.Bool(true),
-	// }
-	// stream, err := client.Streams.Sample(sampleParams)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// Receive messages until stopped or stream quits
-	go demux.HandleChan(stream.Messages)
-
-	// Wait for SIGINT and SIGTERM (HIT CTRL-C)
-	ch := make(chan os.Signal)
-	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-	log.Println(<-ch)
-
-	fmt.Println("Stopping Stream...")
-	stream.Stop()
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
