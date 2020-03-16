1b541a6411182e578050f5dd4d584fac0c2e899d
Dave Augustus <davetweetlive@gmail.com>
2020-03-17 00:56:54 +0530

    Removed everything unnecessary, the application will be rewritten

--- main.go
+++ main.go
@@ -1,22 +1,7 @@
 package main
 
-import (
-	"twitter-stat/controller"
-	// "twitter-stat/model"
-	"log"
-	"net/http"
-
-	"github.com/gorilla/mux"
-)
+import "fmt"
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/register", controller.RegisterHandler).
-		Methods("POST")
-	r.HandleFunc("/login", controller.LoginHandler).
-		Methods("POST")
-	r.HandleFunc("/profile", controller.ProfileHandler).
-		Methods("GET")
-
-	log.Fatal(http.ListenAndServe(":8080", r))
+	fmt.Println("The application is being written!")
 }
