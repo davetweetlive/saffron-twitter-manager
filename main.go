998d84f79492df2b60b6526923fa2974d23b8d40
Dave Augustus <davetweetlive@gmail.com>
2020-04-05 06:11:36 +0530

    able to get html value

--- main.go
+++ main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"twitter-stat/models"
 	"twitter-stat/views"
 )
 
 func main() {
-	db, err := models.EstablishDBConnection()
-	if err != nil {
-		fmt.Println("DB connection denied!")
-	}
-	fmt.Println(db)
+	// db, err := models.EstablishDBConnection()
+	// if err != nil {
+	// 	fmt.Println("DB connection denied!")
+	// }
+	// fmt.Println(db)
 	http.HandleFunc("/login", views.LoginHandler)
 	http.HandleFunc("/signup", views.SignUpHandler)
 	http.ListenAndServe(":8000", nil)
